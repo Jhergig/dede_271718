9662f3e973a4fe8ea5881b300222e7b8e579611d
jhergig <jhergig@gmail.com>
2023-01-12 03:19:36 +0100

    restapi para pedidos y almacenes

--- restapi/api.ts
+++ restapi/api.ts
@@ -36,6 +36,15 @@ const pedidoSchema = new mongoose.Schema({
 
 const Pedido = mongoose.model("pedidos", pedidoSchema);
 
+const almacenSchema = new mongoose.Schema({
+  idProducto: Number,
+  almacen: String,
+  cantidad: Number,
+  zona: String
+})
+
+const Almacen = mongoose.model("almacenes", almacenSchema);
+
 api.get(
     "/users/list",
     async (req: Request, res: Response): Promise<Response> => {
@@ -76,5 +85,40 @@ api.get("/productos/:id",
   }
 );
 
+api.get("/almacenes/:id",
+  async (req: Request, res: Response): Promise<Response> => { 
+    let productos;
+    try {
+    productos = await Almacen.find({idProducto:req.params.id})
+    } catch {
+ 
+    }
+    return res.status(200).send(productos);
+  } 
+);
+
+api.get("/pedidos/:id",
+  async (req: Request, res: Response): Promise<Response> => { 
+    let productos = await Pedido.find({webid:req.params.id});
+    return res.status(200).send(productos);
+  } 
+);
+
+api.post("/pedidos/add", [
+  check('webid').isLength({ min: 1 }).trim().escape(),
+],
+async (req: Request, res: Response): Promise<Response> => {
+  console.log(req.body.webid)
+  let webid = req.body.webid.toString().substring(18).split('.')[0]+req.body.webid.toString().substring(18).split('.')[1];
+  let idProducto = req.body.idProducto;
+  let nombreProducto = req.body.nombreProducto;
+  let cantidad = req.body.cantidad;
+  let precio = req.body.precio;
+  let almacen = req.body.almacen;
+  let envio = req.body.envio;
+  let estado = req.body.estado;
+  new Pedido({webid, idProducto, nombreProducto, cantidad, precio, almacen, envio, estado}).save();
+  return res.sendStatus(200);
+})
 
 export default api;
